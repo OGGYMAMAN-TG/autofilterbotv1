98827eceab51df964047ab912166edd3bd39f54a
irash1234567 <85207400+irash1234567@users.noreply.github.com>
2021-07-29 21:50:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@OGGYMAMAN",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer', url="https://t.me/https:mallumovie11"
                                 )
                         ]
                     ]
